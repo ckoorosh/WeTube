fa2a7fdf75603e0bface34fa93dac15e75aefe59
mtndaghyani <matin.daghyani@gmail.com>
2022-07-23 14:11:24 +0430

    Update video app's models

--- videos/models.py
+++ videos/models.py
@@ -4,24 +4,28 @@ from django.db import models
 from accounts.models import User
 
 
-class Video:
+class Video(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    permitted = models.BooleanField(default=True)  # To see if the video is banned or not
+    banned = models.BooleanField(default=False)  # To see if the video is banned or not
+
+    def ban(self):
+        self.banned = True
+        self.save()
 
 
-class Comment:
+class Comment(models.Model):
     body = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
 
 
-class Tag:
+class Tag(models.Model):
     videos = models.ManyToManyField(Video)
 
 
-class Ticket:
+class Ticket(models.Model):
     TICKET_STATUS = (
         ("n", "new"),
         ("w", "waiting"),
