0d5cd40db6672319cf74f5a27737b57ed912ad7f
mtndaghyani <matin.daghyani@gmail.com>
2022-07-23 13:18:47 +0430

    Add some methods to User model

--- accounts/models.py
+++ accounts/models.py
@@ -7,5 +7,10 @@ class User(AbstractUser):
     strike = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
+    def make_admin(self):
+        self.is_admin = True
+        self.save()
 
-
+    def set_strike(self, strike_status):
+        self.strike = strike_status
+        self.save()
