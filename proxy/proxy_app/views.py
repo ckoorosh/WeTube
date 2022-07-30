dd37ab628fd0990d0a3dffd4771a4063050dc92a
mtndaghyani <matin.daghyani@gmail.com>
2022-07-30 12:22:10 +0430

    Add proxy view

--- proxy/proxy_app/views.py
+++ proxy/proxy_app/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from proxy_app.request_forwarder import RequestForwarder
+
+forwarder = RequestForwarder(scheme='http',
+                             domain='127.0.0.1',
+                             port=8000)
+
+
+def proxy_view(request):
+    content, status_code = forwarder.forward(request)
+    return HttpResponse(content, status=status_code)
