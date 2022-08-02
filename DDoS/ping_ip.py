786657e920fb9ae9ec32accbf04e8328f2dbdb84
mohammadalikakavand <malikakavand@gmail.com>
2022-08-03 01:24:09 +0430

    add ping_ip.py

---
+++ DDoS/ping_ip.py
@@ -0,0 +1,17 @@
+import time
+from datetime import datetime
+
+from pythonping import ping
+
+TIMEOUT = 0.01
+IP = '127.0.0.1'
+PORT = '8000'
+
+while True:
+    time.sleep(1)
+    response_list = ping('127.0.0.1', count=100)
+    status = 'up' if response_list.rtt_avg_ms < TIMEOUT else 'down'
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time is :",current_time, 'and server is', status)
+
