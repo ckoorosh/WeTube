19be3feea407d00651e85ce3642f93a02b039083
mohammadalikakavand <malikakavand@gmail.com>
2022-08-06 23:30:17 +0430

    new code for tester.py

--- DDoS/tester.py
+++ DDoS/tester.py
@@ -1,22 +1,17 @@
-import socket
 import time
-from datetime import datetime
+import requests
 
-TIMEOUT =0.001
-IP = '127.0.0.1'
+TIMEOUT = 6000  # microseconds
+IP = 'http://127.0.0.1:'
 PORT = '8000'
 
 while True:
     time.sleep(1)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(TIMEOUT)
     try:
-        t0 = datetime.now()
-        sock.connect((IP, int(PORT)))
-        now = datetime.now()
-        sock.close()
-
+        x = requests.get(IP + PORT)
+        if x.elapsed.microseconds > TIMEOUT:
+            print('timeout for request')
+        else:
+            print('server is healthy')
     except:
         print('server is down')
-    else:
-        print("connecting time is :"+"{:.2f}".format(round((now - t0).total_seconds()*1000), 2)+ 'ms\tand server is up')
