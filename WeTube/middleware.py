26c408eaf52125286799a21becb27769a411b5d1
mohammadalikakavand <malikakavand@gmail.com>
2022-08-06 23:29:43 +0430

    edit middleware.py add comment and change the threshold

--- WeTube/middleware.py
+++ WeTube/middleware.py
@@ -4,11 +4,18 @@ import time
 from datetime import datetime
 from threading import Thread
 
+"""
+how to find number of requests per second threshold?
+https://security.stackexchange.com/questions/118344/normal-usage-vs-denial-of-service-how-many-requests-are-needed-to-talk-about-a
+simple answer : there is not any specific value for EVERY system.
+"""
+
 class DDoS:
     def __init__(self, get_response):
         self.get_response = get_response
         self.ip_list = {}
-        self.list_safe = []
+        # self.list_safe = ['127.0.0.1']
+        self.list_safe =[]
         Thread(target=self.checker, args=()).start()
 
     def __call__(self, request):
@@ -24,7 +31,7 @@ class DDoS:
         while True:
             attack = False
             for ip, count in self.ip_list.items():
-                if count > 20 and ip not in self.list_safe:
+                if count > 100 and ip not in self.list_safe:
                     attack = True
             if attack:
                 now = datetime.now()
